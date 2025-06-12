661f5da8f7e58ff2d81021db0184304d9c62d268
ravib7 <ravibchaudhari7@gmail.com>
2025-06-12 11:33:18 +0530

    index.js updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://exam-central-useq.vercel.app/", credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", require("./routes/auth.route"))
